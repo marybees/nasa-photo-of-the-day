0edef50bab25738f4aea200b467f5878633144e8
Mary Baker <meb601@gmail.com>
2020-08-12 23:49:43 -0400

    Adjust API key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import "./App.css";
 import axios from 'axios'
-import { BASE_URL, API_KEY } from './constants'
+import { API_KEY } from './constants'
 import Details from './Details'
 
 
 function App() {
+  const [photo, setPhoto] = useState([])
   return (
     <div className="App">
       <h1>NASA Photo of the Day</h1>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,1 @@
-export const BASE_URL = 'http://localhost:3000'
 export const API_KEY = 'https://api.nasa.gov/planetary/apod?api_key=bpJ7TgEPWUCwtiDA3KTp3s6CMzQXVUFF66TfzCIo'
