60a36efdeb2dac03ded559ece41c4007d0d6672b
Mary Baker <meb601@gmail.com>
2020-08-13 17:32:30 -0400

    Revert back to older version and fix looping error

--- src/PhotoCard.js
+++ src/PhotoCard.js
@@ -1,8 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import PhotoList from './PhotoList'
-=======
->>>>>>> parent of 75df4eb... Add click event to hide and show explanation
 
 const PhotoCard = (props) => {
     return (
@@ -11,7 +7,7 @@ const PhotoCard = (props) => {
             <h1>NASA Photo of the Day</h1>
         </div>
         <div className='above-photo'>
-            <h4>Title: {props.title}</h4>
+            <h4>{props.title}</h4>
             <h5>Date: {props.date}</h5>
         </div>
         <div className='photo-container'>
@@ -22,16 +18,13 @@ const PhotoCard = (props) => {
         />
       </div>
         <div className="below-photo">
-<<<<<<< HEAD
+
             <button onClick={props.toggleExplanation}>{props.buttonText}</button>
             <p>{props.explanation}</p>
-=======
-            <button>Show Explanation</button>
-            <p>Explanation: {props.explanation}</p>
->>>>>>> parent of 75df4eb... Add click event to hide and show explanation
+
         </div>
         <div className='footer'>
-            <p>Copyright: {props.copyright}</p>
+            <p>© {props.copyright}</p>
         </div>
     </div>
     );
